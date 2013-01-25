f9d4277ed40943a369fb66d2381f3f139a420d41
henrique <hboregio@gmail.com>
2013-01-25 10:34:22 -0300

    Initial Commit

--- src/com/thirtymatches/fivehundred/Constants.java
+++ src/com/thirtymatches/fivehundred/Constants.java
@@ -2,7 +2,7 @@ package com.thirtymatches.fivehundred;
 
 public class Constants {
 
-	public static final String CONSUMER_KEY = "8WxMFi1wFFRnIcRtVJXJlyxma3qokClKyAw6sUJz";
+	public static final String CONSUMER_KEY = "ADD YOUR API KEY HERE";
 	public static final String DEFAULT_URL = "https://api.500px.com/v1/photos?feature=popular&image_size=2&consumer_key=" + CONSUMER_KEY;
 	public static final String URL_FOR_CATEGORY = "https://api.500px.com/v1/photos?feature=popular&image_size=2&consumer_key=" + CONSUMER_KEY + "&only=";
 
